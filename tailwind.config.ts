ec2fbd4867093cf00bbb499ab2b4f729330478f3
ParkSoHyeon <dso753@naver.com>
2023-11-19 17:32:37 +0900

    tailwind css, postcss settings

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./pages/**/*.{html,js}', './components/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
