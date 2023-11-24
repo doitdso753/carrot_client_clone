19e31e59bf535e287d58fa35ac834fa26b0983bf
ParkSoHyeon <dso753@naver.com>
2023-11-24 17:51:00 +0900

    assets 관련 파일 이동

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './pages/Dashboard/index.css';
+import './assets/styles/index.css';
 import App from './pages/Dashboard/App';
 
 const root = ReactDOM.createRoot(
--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,5 +1,5 @@
 import './index.css';
-import Logo from '../../assets/logo.svg';
+import Logo from '../../assets/images/logo.svg';
 
 function Header() {
   return (
